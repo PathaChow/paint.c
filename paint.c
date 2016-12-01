1f029102a933beb5c4917b42540e1dfbac0226f1
Mamkh27 <mkheer@ucdavis.edu>
2016-12-01 13:57:13 -0800

    Update paint.c

--- paint.c
+++ paint.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-quit(char** canvas, char c);
+
+char** makeCanvas (int numRows, int numCols);
+void printCanvas(int numRows, int numCols, char** canvas);
 
 
+void setUp (char*** canvas, int numRows, int numCols){
+	*canvas = makeCanvas(numRows, numCols);
+	return; 
+}
 
 void printCanvas(int numRows, int numCols, char** canvas){
-
-for (i = 0 ; i < numRows; i++){
-	printf("%d", numRows - i -1)
-	for(j = 0; j < numCols; j++){
-		printf("%c", canvas[i][j]);
+int i;
+int j;
+for (i = 0 ; i < numRows; ++i){
+	printf("%d", numRows - i -1);
+	for(j = 0; j < numCols; ++j){
+		printf(" %c", canvas[i][j]);
 		       }
 	printf("\n");
 }
-printf("  "); 	 
-for (j = 0 ; j < numCols, ; j++){
-	printf("%d", j)
+printf(" "); 	 
+for (j = 0 ; j < numCols; ++j){
+	printf(" %d", j);
 }
-return 0; 
+return; 
 }
 		
 		
-char** makeCanvas (numRows, int numCols){	
-char** canvas = (char** malloc(numRows * sizeof(char*)); 
+char** makeCanvas (int numRows, int numCols){	
+char** canvas = (char**) malloc(numRows * sizeof(char*)); 
 		 
 int i;
 int j; 
 		 	 
-for (i = 0; i < numRows; i++){
-	canvas[i] = (char*)malloc(numCols *sizeof(char));
-	for (j = 0; j < numCols; j++){
-	
-	canvas[i][j] = '*'; 
+for (i = 0; i < numRows; ++i){
+	(canvas)[i] = (char*) malloc(numCols *sizeof(char));
+	for (j = 0; j < numCols; ++j){
+	(canvas)[i][j] = '*'; 
 	}
 }		 		 
 return canvas; 
@@ -39,44 +46,26 @@ return canvas;
 
 		 
 		 
-int main(argc, argv[]){
+int main(int argc, char* argv[]){
+	int numRows;
+	int numCols;
 	
-	
-	if (!argv[2]){
+	if (argc < 2){
 		numRows = 10; 
 		numCols = 10; 
 	} else {
 		numRows = atoi(argv[1]);
 		numCols = atoi(argv[2]);
-	}
-	char*** canvas; 
-	printCanvas();
-	makeCanvas();
-	input(); 
-	cleanup(); 
+	} 
+
+	 
+	char** canvas; 
+	
+	setUp(&canvas, numRows, numCols);
+	printCanvas(numRows, numCols, canvas);
+	
+	//input(); 
+	//cleanup(); 
 	
 	return 0;
-}
-		 
-		 
-		 
-
-void input(){
-	
-quit(char** canvas, char c);
-help();
-
-		
-}
-
-		 
-		 
-		 
-		 
-void quit(char** canvas, char c){		 
-	if(c == "q"){
-		free(canvas);
-		exit (0);
-}
-return; 
 }
